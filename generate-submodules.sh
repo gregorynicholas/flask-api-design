3bfa07055464375b7d2138f2dc42ca8a1d94995e
Gregory Nicholas <nicholas.g.gregory@gmail.com>
2013-07-07 11:14:03 -0500

    adds the primitive script i used to add all of the submodules

---
+++ generate-submodules.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+git submodule add https://github.com/ahri/flask-snooze.git projects/ahri-flask-snooze
+git submodule add https://github.com/twilio/flask-restful.git projects/twilio-flask-restful
+git submodule add https://github.com/flosch/simpleapi.git projects/flosch-simpleapi
+git submodule add https://github.com/jfinkels/flask-restless.git projects/jfinkels-flask-restless
+git submodule add https://github.com/hownowstephen/apy.git projects/hownowstephen-apy
+git submodule add https://github.com/dontcare4free/Flask-Pushrod.git projects/dontcare4free-flask-pushrod
+git submodule add https://github.com/ib-lundgren/flask-oauthprovider.git projects/ib-lundgren-flask-oauthprovider
+git submodule add https://github.com/martinblech/mimerender.git projects/martinblech-mimerender
+git submodule add https://github.com/shazow/apiclient.git projects/shazow-apiclient
+git submodule add https://github.com/dstufft/slumber.git projects/dstufft-slumber
+git submodule add https://github.com/namlook/apibee.git projects/namlook-apibee
+git submodule add https://github.com/scotch/aerest.git projects/scotch-aerest
+git submodule add https://github.com/jacobb/nap.git projects/jacobb-nap
+git submodule add https://github.com/mattupstate/flask-negotiate.git projects/mattupstate-flask-negotiate
+git submodule add https://github.com/garybernhardt/prest.git projects/garybernhardt-prest
+git submodule add https://github.com/becomingGuru/python-restconsumer.git projects/becomingguru-python-restconsumer
+git submodule add https://github.com/hiidef/hiitrack-api.git projects/hiidef-hiitrack-api
+
